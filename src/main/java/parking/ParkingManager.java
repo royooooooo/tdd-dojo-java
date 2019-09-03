3ac01a8f2add4828c990e0529377aa29940888b6
Fu Yi <yifu@thoughtworks.com>
2019-09-03 23:27:52 +0800

    parking manager can parking by self.

--- src/main/java/parking/ParkingManager.java
+++ src/main/java/parking/ParkingManager.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 public class ParkingManager {
     private List<ParkingBoy> parkingBoys;
-    public ParkingManager(List<ParkingBoy> parkingBoys) {
+    private List<ParkingLot> parkingLots;
+
+    public ParkingManager(List<ParkingBoy> parkingBoys, List<ParkingLot> parkingLots) {
         this.parkingBoys = parkingBoys;
+        this.parkingLots = parkingLots;
     }
 
     public void parkBySelf(Car car) {
-        parkingBoys.get(0).parking(car);
+        parkingLots.get(0).park(car);
     }
 
     public void parkByBoy(Car car) {
--- src/test/java/parking/ParkingManagerTest.java
+++ src/test/java/parking/ParkingManagerTest.java
@@ -3,20 +3,27 @@ package parking;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class ParkingManagerTest {
     @Test
-    void should_manage_parking_boy() {
+    void should_parking_car_by_parking_boy() {
         List<ParkingLot> parkingLots = new ArrayList<>();
         parkingLots.add(new ParkingLot());
         ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
 
         List<ParkingBoy> parkingBoys = new ArrayList<>();
         parkingBoys.add(parkingBoy);
-        ParkingManager parkingManager = new ParkingManager(parkingBoys);
+        ParkingManager parkingManager = new ParkingManager(parkingBoys, Collections.emptyList());
         parkingManager.parkBySelf(new Car());
         parkingManager.parkByBoy(new Car());
+    }
 
+    @Test
+    void should_parking_car_by_self() {
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingManager parkingManager = new ParkingManager(Collections.emptyList(), parkingLots);
+        parkingManager.parkBySelf(new Car());
     }
 }
